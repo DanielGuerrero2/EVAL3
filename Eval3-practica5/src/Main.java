b6edc229c5f53eb5560d736f5ae0d188718550b8
Daniel Reza <porta11271127@gmail.com>
2024-11-07 17:32:46 -0600

    new features

--- Eval3-practica5/src/Main.java
+++ Eval3-practica5/src/Main.java
@@ -2,11 +2,24 @@ public class Main {
     public static void main(String[] args) {
 
         String [] data = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
+        String  []  meses = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto" , "septiembre","Octubre", "Noviembre", "Diciembre"};
+        int [] billetes ={20,50,100,200,500,1000};
         printData(data);
+        printData(meses);
+        printInt(billetes);
+
     }
+
     public static void printData(String [] data) {
         for (String s : data) {
             System.out.println(s);
         }
     }
+
+    public static void printInt(int [] data) {
+        for (int i : data) {
+            System.out.println(i);
+        }
+    }
+
 }
